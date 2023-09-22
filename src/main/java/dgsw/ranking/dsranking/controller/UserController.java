694854582c69a7a635efb63595eabbefa54b37f8
HINU0622 <HINU0622@gmail.com>
2023-09-22 11:13:26 +0900

    기본 설정

--- src/main/java/dgsw/ranking/dsranking/controller/UserController.java
+++ src/main/java/dgsw/ranking/dsranking/controller/UserController.java
@@ -1,4 +1,14 @@
 package dgsw.ranking.dsranking.controller;
 
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/user")
 public class UserController {
+
+
+
 }

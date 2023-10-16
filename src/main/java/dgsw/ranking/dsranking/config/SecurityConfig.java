b366fa7150a3dd2cc4ec3cefbca62baed97c8fd8
HINU0622 <HINU0622@gmail.com>
2023-10-16 14:40:23 +0900

    OAuth2 유저 정보 받도록 수정

--- src/main/java/dgsw/ranking/dsranking/config/SecurityConfig.java
+++ src/main/java/dgsw/ranking/dsranking/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package dgsw.ranking.dsranking.config;
 
+import dgsw.ranking.dsranking.security.DsRankingOAuth2UserService;
+import dgsw.ranking.dsranking.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,13 +11,19 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final DsRankingOAuth2UserService oauth2Service;
+
+    private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+
+    private UserService userService;
 
     @Bean
     protected SecurityFilterChain config(HttpSecurity http) throws Exception {
         http.oauth2Login(oauth2 ->
-                oauth2.authorizationEndpoint(auth ->
-                        auth.baseUri("/login")));
+                oauth2.successHandler(oAuth2AuthenticationSuccessHandler));
         return http.build();
     }
 

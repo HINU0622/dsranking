d28a27ab0399f2ee3ef3d5ff8076c3369555f85d
HINU0622 <HINU0622@gmail.com>
2023-10-16 14:11:40 +0900

    OAuth2Attributes 작성

---
+++ src/main/java/dgsw/ranking/dsranking/config/OAuth2Config.java
@@ -0,0 +1,31 @@
+package dgsw.ranking.dsranking.config;
+
+import dgsw.ranking.dsranking.security.DsRankingUserDetails;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class OAuth2Config extends SimpleUrlAuthenticationSuccessHandler {
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                        Authentication authentication) throws IOException, ServletException {
+
+        DsRankingUserDetails userDetails = (DsRankingUserDetails)authentication.getPrincipal();
+
+        log.info("[INFO - SUCCESS] {}", userDetails);
+
+
+
+    }
+}

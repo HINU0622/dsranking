32ed232bf1c066626da5aa4d84fade4cf7778931
HINU0622 <HINU0622@gmail.com>
2023-10-16 14:11:22 +0900

    UserDetails 작성

---
+++ src/main/java/dgsw/ranking/dsranking/security/DsRankingUserDetails.java
@@ -0,0 +1,81 @@
+package dgsw.ranking.dsranking.security;
+
+import dgsw.ranking.dsranking.domain.User;
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
+@Getter
+@ToString
+public class DsRankingUserDetails implements UserDetails, OAuth2User {
+
+    private User user;
+
+    private Map<String, Object> attributes;
+
+    public DsRankingUserDetails(User user) {
+        this(user, null);
+    }
+
+    public DsRankingUserDetails(User user, Map<String, Object> attributes) {
+        this.user = user;
+        this.attributes = attributes;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public String getName() {
+        return user.getEmail();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+
+        authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
+}
